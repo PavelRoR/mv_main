7bfb2e96902fe5a1a03be289d594459f5c98bfb4
pavelp21 <pavelvp2182@yandex.ru>
2018-05-11 16:51:33 +0500

    Main is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-
-
+    // $("#main_slider").owlCarousel({
+    //     autoPlay : 3000,
+    //     stopOnHover : true,
+    //     navigation:true,
+    //     paginationSpeed : 1000,
+    //     goToFirstSpeed : 2000,
+    //     Item : 3,
+    //     autoHeight : true,
+    //     transitionStyle:"fade"
+    // });
 
 /*Конец документа*/
 });
