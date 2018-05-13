a846a1e39cb85a718b9564ad4efec4f7a82baf2e
pavelp21 <pavelvp2182@yandex.ru>
2018-05-13 18:18:07 +0500

    FAQ is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-    // $("#main_slider").owlCarousel({
-    //     autoPlay : 3000,
-    //     stopOnHover : true,
-    //     navigation:true,
-    //     paginationSpeed : 1000,
-    //     goToFirstSpeed : 2000,
-    //     Item : 3,
-    //     autoHeight : true,
-    //     transitionStyle:"fade"
-    // });
+
 /* Видео */
 $(".video_wrapper").click(function () {
     var a = $(this).attr("data-youtube");
@@ -18,5 +9,26 @@ $(".about_esoteric_title").click(function () {
     var a = $(this).next().attr("data-youtube");
     $(this).next().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allowfullscreen></iframe>')
 });
+
+/*FAQ*/ 
+$('.faq_container').on('click', function(){
+        var a = $('.faq_title span', this);
+        // $('.faq_title span').text('+');
+        // $('.faq_content').slideUp(500);
+    if (a.text() == '+') {
+        a.text('-');
+        $('.faq_content',this).slideDown(500);
+    }
+    else {
+        a.text('+');
+        $('.faq_content',this).slideUp(500);
+
+    }
+
+
+
+});
+
+
 /*Конец документа*/
 });
