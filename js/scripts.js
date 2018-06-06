2739b2aa779c7b43d45eaa186ec415bdbcd21bde
pavelp21 <pavelvp2182@yandex.ru>
2018-06-06 19:18:40 +0500

    sliders is connected

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,94 @@
 $(document).ready(function() {
+    $("#main_slider").lightSlider({
+        item: 2,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 20,
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        controls: false,
+        easing: 'linear', //'for jquery animation',////
+        speed: 2000, //ms'
+        auto: false,
+        loop: false,
+        slideEndAnimation: false,
+        pause: 5000,
+        pauseOnHover: true,
+        keyPress: false,
+        rtl:false,
+        adaptiveHeight:true,
+        vertical:false,
+        pager: false,
+        gallery: false,
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+    });
+    $("#cir_slider").lightSlider({
+        item: 3,
+        autoWidth: false,
+        slideMove: 1, 
+        // slideMargin: 60,
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        // easing: 'linear',
+        controls: true,
+        prevHtml: '&larr;',
+        nextHtml: '&rarr;',
+        speed: 1000, //ms'
+        auto: false,
+        loop: false,
+        slideEndAnimation: !0,
+        pause: 3000,
+        pauseOnHover: true,
+        keyPress: false,
+        rtl:false,
+        adaptiveHeight:true,
+        vertical:false,
+        pager: false,
+        gallery: false,
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        // swipeThreshold: 40,
+    });
+    $("#last_posts_slider").lightSlider({
+        item: 3,
+        autoWidth: false,
+        slideMove: 1, 
+        // slideMargin: 60,
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        // easing: 'linear',
+        controls: true,
+        prevHtml: '&larr;',
+        nextHtml: '&rarr;',
+        speed: 1000, //ms'
+        auto: false,
+        loop: false,
+        slideEndAnimation: !0,
+        pause: 3000,
+        pauseOnHover: true,
+        keyPress: false,
+        rtl:false,
+        adaptiveHeight:true,
+        vertical:false,
+        pager: false,
+        gallery: false,
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        // swipeThreshold: 40,
+    });
 
 /* Видео */
-$(".video_wrapper").click(function () {
-    var a = $(this).attr("data-youtube");
-    $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  class="video_testimonial" allowfullscreen></iframe>')
+$("#about_eso_video_container ").click(function () {
+    var a = $('.video_wrapper',this).attr("data-youtube");
+    $('.video_wrapper',this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  class="video_testimonial" allowfullscreen></iframe>').css("z-index","19");
 });
 $(".about_esoteric_title").click(function () {
     var a = $(this).next().attr("data-youtube");
